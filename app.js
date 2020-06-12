ff633a32c47a8a2ef437217c2c1f5b888176d1f9
Lalit Kumar Singh <lalit3370@gmail.com>
2020-06-12 20:10:48 +0530

    added signup,login page

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-var path=require('path');
-var express=require('express');
-var bodyParser=require('body-parser');
-var cookieParser=require('cookie-parser');
-var expressValidator=require('express-validator');
-var flash=require('connect-flash'); //not sure y used
-var session=require('express-session');
-var passport =require('passport');
-var LocalStrategy=require('passport-local');
-var mongo=require('mongodb');
-var mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://lalit:lalit123@cluster0-fvaxm.gcp.mongodb.net/animenow', { useNewUrlParser: true ,useUnifiedTopology: true })
-var db=mongoose.connection;
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash'); //not sure y used
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://lalit:lalit123@cluster0-fvaxm.gcp.mongodb.net/animenow', { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection;
 
-var homeRoutes=require('./routes/home');
-var authRoutes=require('./routes/auth');
+var homeRoutes = require('./routes/home');
+var authRoutes = require('./routes/auth');
 
-var app=express();
-
+var app = express();
+// app.use(flash());
 app.set('view engine', 'ejs');
-app.set('views','views');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(session({
-    secret:'secret',
+    secret: 'secret',
     saveUninitialized: true,
     resave: true
 }));
+// app.use(function (req, res, next) {
+//     res.locals.success_msg = req.flash('success_msg');
+//     res.locals.error_msg = req.flash('error_msg');
+//     res.locals.error = req.flash('error');
+//     next();
+// });
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(flash());
+
 app.use(authRoutes);
 app.use(homeRoutes);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,29 @@
 var express=require('express');
 var router=express.Router();
 const path = require('path');
-router.post('/signup', (req, res) => {
-    console.log('ligma');
+
+router.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname,'../','views','signup.html'));
 });
+const { check, validationResult } = require('express-validator');
+
+router.post('/signup',[
+    check('username').notEmpty(),
+    check('userpass').notEmpty()
+  ],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    var username= req.body.username;
+    var password= req.body.userpass;
+    console.log(password);
+});
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'../','views','login.html'));
+});
 router.get((req,res,next)=>{
-    console.log('404');
     res.status(404).render('404',{pageTitle: 'Page not found'});
 });
+
 module.exports=router;
-//   User.create({
-//     username: req.body.username,
-//     password: req.body.password
-//   }).then(user => res.json(user));
-// });
-// const { check, validationResult } = require('express-validator');
-
-// router.post('/signup', [
-//   // username must be an email
-//   check('username').isEmail(),
-//   // password must be at least 5 chars long
-//   check('password').isLength({ min: 5 })
-// ], (req, res) => {
-//   // Finds the validation errors in this request and wraps them in an object with handy functions
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(422).json({ errors: errors.array() });
-//   }
-
-//   User.create({
-//     username: req.body.username,
-//     password: req.body.userpass
-//   }).then(user => res.json(user));
