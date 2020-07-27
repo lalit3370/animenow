3bbb5cee50996c87aa98a05c5359f9590cc796e5
Lalit Kumar Singh <lalit3370@gmail.com>
2020-07-28 04:39:10 +0530

    fized swiper, switched to anilist api

--- public/home.js
+++ public/home.js
@@ -2,107 +2,7 @@ getnew();
 gettrend();
 gettop();
 
-// function getnew() {
-//     let todaydate = new Date();
-//     let year = todaydate.getFullYear();
-//     let month = todaydate.getMonth() + 1;
-//     season = ['winter', 'winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'fall', 'fall', 'fall'];
-//     let requestURL = 'https://api.jikan.moe/v3/season/' + year + '/' + season[month];
-//     // let requestURL='/jsonfiles/new.json'
-//     fetch(requestURL)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((newdata) => {
-//             var d1 = document.getElementById('newanime');
-//             if (typeof animelist != 'undefined') {
-//                 for (var i = 0; i < 50; i++) {
-//                     var element = newdata.anime[i];
-//                     if (animelist.some(function getelement(list) {
-//                         return list == element.mal_id;
-//                     })) {
-//                         console.log("found " + element.mal_id);
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     } else {
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     }
-//                 }
-//             }
-//             else {
-//                 for (var i = 0; i < 50; i++) {
-//                     const element = newdata.anime[i];
-//                     d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                 }
-//             }
-
-//         });
-// }
-// function gettrend() {
-//     let requestURL = 'https://api.jikan.moe/v3/top/anime/1/airing';
-//     fetch(requestURL)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((trenddata) => {
-
-//             var d1 = document.getElementById('trendanime');
-
-//             if (typeof animelist != 'undefined') {
-
-//                 for (var i = 0; i < 50; i++) {
-//                     var element = trenddata.top[i];
-//                     if (animelist.some(function getelement(list) { return list == element.mal_id; })) {
-//                         console.log("found " + element.mal_id);
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     }
-//                     else {
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     }
-//                 }
-//             }
-//             else {
-//                 for (var i = 0; i < 50; i++) {
-//                     const element = trenddata.top[i];
-//                     d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                 }
-//             }
-//         });
-// }
-
-// function gettop() {
-//     let requestURL = 'https://api.jikan.moe/v3/search/anime?order_by=score';
-//     // let requestURL='/jsonfiles/top.json'
-//     fetch(requestURL)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((topdata) => {
-            // var d1 = document.getElementById("topanime");
-
-//             if (typeof animelist != 'undefined') {
-
-//                 for (var i = 0; i < 50; i++) {
-//                     var element = topdata.results[i];
-//                     if (animelist.some(function getelement(list) {
-//                         return list == element.mal_id;
-//                     })) {
-//                         console.log("found " + element.mal_id);
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     } else {
-//                         d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                     }
-//                 }
-//             }
-//             else {
-//                 for (var i = 0; i < 50; i++) {
-//                     const element = topdata.results[i];
-//                     d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-//                 }
-//             }
-//         });
-// }
 function getnew(){
-    var d1 = document.getElementById("newanime");
     var query = `
     query ($id: Int, $page: Int, $perPage: Int, $search: String, $sort: [MediaSort], $isAdult: Boolean, $status: MediaStatus, $format_not: MediaFormat) {
       Page (page: $page, perPage: $perPage) {
@@ -120,8 +20,7 @@ function getnew(){
         coverImage {
           large
         }
-        status
-        format
+        idMal
         }
       }
     }
@@ -151,19 +50,30 @@ var url = 'https://graphql.anilist.co',
 fetch(url, options)
   .then(handleResponse)
   .then((data)=>{
-    console.log(data);
+    var d1 = document.getElementById("newanime");
     var count = Object.keys(data.data.Page.media).length;
-    for (var i = 0; i < count; i++) {
+    if (typeof animelist != 'undefined') {
+      for (var i = 0; i < count; i++) {
         const element=data.data.Page.media[i];
-          d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');    
+          if (animelist.some(function getelement(list) {
+              return list == element.idMal;
+          })) {
+              d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+          } else {
+              d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+          }
+      }
     }
-  })
-  .catch(handleError);
-
+    else {
+        for (var i = 0; i < count; i++) {
+          const element=data.data.Page.media[i];
+            d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji+ '</p></div></div>');
+        }
+    }
+}).catch(handleError);
 }
 
 function gettrend(){
-  var d1 = document.getElementById("trendanime");
   var query = `
   query ($id: Int, $page: Int, $perPage: Int, $search: String, $sort: [MediaSort]) {
     Page (page: $page, perPage: $perPage) {
@@ -212,10 +122,25 @@ fetch(url, options)
 .then(handleResponse)
 .then((data)=>{
   var count = Object.keys(data.data.Page.media).length;
-  for (var i = 0; i < count; i++) {
-      const element=data.data.Page.media[i];
-      d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');    
-  }
+  var d1 = document.getElementById('trendanime');
+    if (typeof animelist != 'undefined') {
+
+        for (var i = 0; i < count; i++) {
+          const element=data.data.Page.media[i];
+            if (animelist.some(function getelement(list) { return list == element.idMal; })) {
+                d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+            }
+            else {
+                d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+            }
+        }
+    }
+    else {
+        for (var i = 0; i < count; i++) {
+          const element=data.data.Page.media[i];
+            d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+        }
+    }
 })
 .catch(handleError);
 
@@ -277,14 +202,30 @@ fetch(url, options)
   .then(handleResponse)
   .then((data)=>{
     var count = Object.keys(data.data.Page.media).length;
-    for (var i = 0; i < count; i++) {
-        const element=data.data.Page.media[i];
-        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');    
+    var d1 = document.getElementById("topanime");
+
+    if (typeof animelist != 'undefined') {
+
+        for (var i = 0; i < count; i++) {
+          const element=data.data.Page.media[i];
+            if (animelist.some(function getelement(list) {
+                return list == element.idMal;
+            })) {
+                d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+            } else {
+                d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+            }
+        }
+    }
+    else {
+        for (var i = 0; i < count; i++) {
+          const element=data.data.Page.media[i];
+            d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
+        }
     }
   })
   .catch(handleError);
 }
-
 
 function handleResponse(response) {
     return response.json().then(function (json) {
@@ -295,3 +236,52 @@ function handleResponse(response) {
 function handleError(error) {
     console.error(error);
 }
+var swiper1 = new Swiper('.swiper-container.new', {
+  slidesPerView: 6,
+  spaceBetween: -50,
+  slidesPerGroup: 6,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+var swiper2 = new Swiper('.swiper-container.trend', {
+  slidesPerView: 6,
+  spaceBetween: -50,
+  slidesPerGroup: 6,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper3 = new Swiper('.swiper-container.top', {
+  slidesPerView: 6,
+  spaceBetween: -50,
+  slidesPerGroup: 6,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+setTimeout(() => {
+  swiper1.update();
+  swiper2.update();
+  swiper3.update();
+  console.log("now");
+}, 1500);
