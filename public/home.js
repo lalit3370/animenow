27474ef1ea2667dce2e3ac793f400ec645060453
Lalit Kumar Singh <lalit3370@gmail.com>
2020-07-29 03:44:09 +0530

    .

--- public/home.js
+++ public/home.js
@@ -2,40 +2,6 @@ getnew();
 gettrend();
 gettop();
 
-<<<<<<< HEAD
-function getnew() {
-    let todaydate = new Date();
-    let year = todaydate.getFullYear();
-    let month = todaydate.getMonth() + 1;
-    season = ['winter', 'winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'fall', 'fall', 'fall'];
-    // let requestURL = 'https://api.jikan.moe/v3/season/' + year + '/' + season[month];
-    let requestURL = 'jsonfiles/new.json';
-    fetch(requestURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((newdata) => {
-            var d1 = document.getElementById('newanime');
-            if (typeof animelist != 'undefined') {
-                for (var i = 0; i < 50; i++) {
-                    var element = newdata.anime[i];
-                    if (animelist.some(function getelement(list) {
-                        return list == element.mal_id;
-                    })) {
-                        console.log("found " + element.mal_id);
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    } else {
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    }
-                }
-            }
-            else {
-                for (var i = 0; i < 50; i++) {
-                    const element = newdata.anime[i];
-                    d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                }
-            }
-=======
 function getnew(){
     var query = `
     query ($id: Int, $page: Int, $perPage: Int, $search: String, $sort: [MediaSort], $isAdult: Boolean, $status: MediaStatus, $format_not: MediaFormat) {
@@ -59,7 +25,6 @@ function getnew(){
       }
     }
   `;
->>>>>>> 131f53521e7df92a66bb49905892077debe744b9
 
 var variables = {
   sort: "START_DATE_DESC",
@@ -107,31 +72,6 @@ fetch(url, options)
     }
 }).catch(handleError);
 }
-<<<<<<< HEAD
-function gettrend() {
-    // let requestURL = 'https://api.jikan.moe/v3/top/anime/1/airing';
-    let requestURL = 'jsonfiles/trend.json';
-    fetch(requestURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((trenddata) => {
-
-            var d1 = document.getElementById('trendanime');
-
-            if (typeof animelist != 'undefined') {
-
-                for (var i = 0; i < 50; i++) {
-                    var element = trenddata.top[i];
-                    if (animelist.some(function getelement(list) { return list == element.mal_id; })) {
-                        console.log("found " + element.mal_id);
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    }
-                    else {
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    }
-                }
-=======
 
 function gettrend(){
   var query = `
@@ -189,7 +129,6 @@ fetch(url, options)
           const element=data.data.Page.media[i];
             if (animelist.some(function getelement(list) { return list == element.idMal; })) {
                 d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
->>>>>>> 131f53521e7df92a66bb49905892077debe744b9
             }
             else {
                 d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
@@ -207,37 +146,6 @@ fetch(url, options)
 
 }
 
-<<<<<<< HEAD
-function gettop() {
-    // let requestURL = 'https://api.jikan.moe/v3/search/anime?order_by=score';
-    let requestURL = 'jsonfiles/top.json';
-    fetch(requestURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((topdata) => {
-            var d1 = document.getElementById("topanime");
-
-            if (typeof animelist != 'undefined') {
-
-                for (var i = 0; i < 50; i++) {
-                    var element = topdata.results[i];
-                    if (animelist.some(function getelement(list) {
-                        return list == element.mal_id;
-                    })) {
-                        console.log("found " + element.mal_id);
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    } else {
-                        d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.mal_id + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                    }
-                }
-            }
-            else {
-                for (var i = 0; i < 50; i++) {
-                    const element = topdata.results[i];
-                    d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"><img src=' + element.image_url + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title + '</p></div></div>');
-                }
-=======
 function gettop(){
     var d1 = document.getElementById("topanime");
     var query = `
@@ -306,7 +214,6 @@ fetch(url, options)
                 d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/remove" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-check-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
             } else {
                 d1.insertAdjacentHTML('beforeend', '<div class="swiper-slide"><div class="animethumbs"> <form action="/add" method="post"><button type="submit" class="addtoprofile"  name="animeid" value=' + element.idMal + ' onclick="addtoprofile()"> <i class="fas fa-plus-square fa-3x"></i></button></form><img src=' + element.coverImage.large + ' alt="Error Displaying the image"><div class="imagetext"><p>' + element.title.romaji + '</p></div></div>');
->>>>>>> 131f53521e7df92a66bb49905892077debe744b9
             }
         }
     }
