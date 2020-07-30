bd6719d7792409b0986c820d4ec21c3b17ee741c
Lalit Kumar Singh <lalit3370@gmail.com>
2020-07-31 03:09:11 +0530

    new homepage ss

--- config/checkauth.js
+++ config/checkauth.js
@@ -4,7 +4,6 @@ module.exports = {
         return next();
       }
       req.flash('error_msg', 'Log in to view that page');
-      req.session.returnTo = req.originalUrl; 
       res.redirect('/login');
     },
     forwardAuthenticated: function(req, res, next) {
