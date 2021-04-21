02ddd685481341c9006c626e79ad7e0b4b4efe6f
lalit3370 <44034501+lalit3370@users.noreply.github.com>
2021-04-21 23:10:55 +0530

    censor

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://lalit:lalit123@cluster0-fvaxm.gcp.mongodb.net/animenow",
+    "mongodb+srv://<username>:<password>@cluster0-fvaxm.gcp.mongodb.net/animenow",
 };
